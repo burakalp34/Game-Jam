74fe8c8f3cc9cfb5551129768e15cdb225461fe4
burakalp34 <bakthea@hotmail.com>
2025-11-08 23:54:03 +0100

    Planet Generation

--- Scripts/SpaceshipController.gd
+++ Scripts/SpaceshipController.gd
@@ -26,6 +26,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#print($".".position)
+	
 	if(thrustersFiring):
 		$".".apply_force(-transform.y * thrust)
 	
